6589e5d7075332f709f8f4d4e5454db03f3b2ee7
Vladimiriskakov <iskakov.vova@bk.ru>
2025-09-21 17:55:54 +0700

    small changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import App from './app/App.tsx'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend';
